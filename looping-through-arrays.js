9a76751852f25bd76b1f251fb25ed662b95ff8a4
Fatima <xfatimakx@gmail.com>
2022-05-17 21:41:58 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,12 @@
+let pets = ['cat','dog','rat'] 
+let length = pets.length
+let total = 0 
+
+
+for (let i = total; i < length; i++)
+{total += i
+pets[i] = pets[i] + 's'  
+console.log(pets[i])}
+
+
+ 
